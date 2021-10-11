10e21ef032b23e3ff33cf256233f8dcbf89cc3be
DarklMoon <92285604+DarklMoon@users.noreply.github.com>
2021-10-11 23:27:07 +0700

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -2,7 +2,10 @@
 
 def main():
     """TEST"""
-    test = ["Check", "GitHub"]
-    print(test[0], test[1]) # Output >>> test
+    msg = input("Put some msg here. >>>")
+    test = ["Check", "GitHub", "Input >>>>"]
+    test.append(msg)
+    print(test[0], test[-1]) # Output >>> test
+    
 
 main()
