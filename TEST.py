c3011fd24caf2ed4e3acb71593684eebeeea518e
DarklMoon <92285604+DarklMoon@users.noreply.github.com>
2021-10-12 12:24:20 +0700

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -6,15 +6,6 @@ def main():
     test = ["Check", "GitHub"]
     print(test[0], test[1]) # Output >>> test
 
-#main()
+main()
 
 #test
-
-"""Can I do this?"""
-
-def check():
-    """test"""
-    keep = {pSit : "code = #@ds0$90"}
-    print(keep[input()])
-    
-check()
